9390f1cbcab876657db86939680cf68c7774bc43
Shabair <shabair143@gmail.com>
2025-11-25 10:12:37 +1000

    config: absolute path, Add: basic product query

---
+++ src/hooks/products/query/index.ts
@@ -0,0 +1,14 @@
+import { useQuery } from "@tanstack/react-query";
+// import { api } from "../../../lib/api/axios";
+import { api } from "@/lib/api/axios";
+
+
+export function useProducts() {
+  return useQuery({
+    queryKey: ["products"],
+    queryFn: async () => {
+      const res = await api.get("/products");
+      return res.data; // Axios returns data directly
+    },
+  });
+}
